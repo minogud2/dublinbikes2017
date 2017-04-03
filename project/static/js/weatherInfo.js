13e581e4633b5c62889335db4a8783312b4c5a22
minogud2 <darragh.minogue@ucdconnect.ie>
2017-04-03 16:46:46 +0100

    weather working

--- project/static/js/weatherInfo.js
+++ project/static/js/weatherInfo.js
@@ -5,13 +5,8 @@ $(document).ready(function(){
     function getData(){
         var data;
 
-     $.getJSON("http://127.0.0.1:5000/weather", null, function(data){
-          if ('weather' in data) {
-              var data = data.dbdata; 
-              // console.log(weather)
-            }
-        
-       
+    $.getJSON('http://127.0.0.1:5000/weather', null, function(d){
+        var data = d.weather;
         var weatherID = data[0][1];
         var weatherTemp = data[0][2];
         var weatherDesc = data[0][3];
@@ -29,37 +24,33 @@ $(document).ready(function(){
 function changeWeatherIcon(weatherType) {
 
   weatherType = weatherType.toLowerCase();
-  $("#weather").text("");
-  $("#weather").append("<i></i>");
+  $("#wIcon").text("");
+  $("#wIcon").append("<i></i>");
 
   if (weatherType.indexOf("clouds") != -1) {
     return $("#wIcon").addClass("wi wi-cloudy");
   } else if (weatherType.indexOf("rain") != -1) {
-    return $("wIcon").addClass("wi wi-rain");
+    return $("#wIcon").addClass("wi wi-rain");
   } else if (weatherType.indexOf("thunderstorm") != -1) {
-    return $("wIcon").addClass("wi wi-thunderstorm");
+    return $("#wIcon").addClass("wi wi-thunderstorm");
   } else if (weatherType.indexOf("snow") != -1) {
-      return $("wIcon").addClass("wi wi-snow");
+    return $("#wIcon").addClass("wi wi-snow");
   } else if (weatherType.indexOf("mist") != -1) {
-    return $("wIcon").addClass("wi wi-smoke");
+    return $("#wIcon").addClass("wi wi-smoke");
   } else {
-    return $("wIcon").addClass("wi wi-day-sunny");
+    return $("#wIcon").addClass("wi wi-day-sunny");
   }
-
 }
 
 function titleCase(str) {
-
   var array = str.split(" ");
 
   for (var i = 0; i < array.length; i++) {
-
     var temp_array = array[i].split(''); // "ab" => "a","b"
     temp_array[0] = temp_array[0].toUpperCase(); // "a","b" => "A","b"
 
     for (var j = 1; j < temp_array.length; j++)
       temp_array[j] = temp_array[j].toLowerCase(); // "a","b" => "A","b"
-
     array[i] = temp_array.join(''); // "A","b" => "Ab"
   }
 
